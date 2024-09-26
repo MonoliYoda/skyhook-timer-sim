8fc45184aab520a0d28d59d2365fd7815b75e0a4
MonoliYoda <48124492+MonoliYoda@users.noreply.github.com>
2024-09-26 17:44:08 +0200

    Add analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Analytics } from "@vercel/analytics/react"
 
 import '@radix-ui/themes/styles.css';
 
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
 
